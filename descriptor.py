dfd6b92b9219c4d786400469e6b9be1386f74b23
MAurice Schleussinger <Maurice.Schleussinger@uni-duesseldorf.de>
2013-03-04 20:49:08 +0100

    whoops

--- descriptor.py
+++ descriptor.py
@@ -1,25 +1,46 @@
-from Tkinter import *
-root = Tk()
-menu = Menu(root)
-root.config(menu=menu)
-filemenu = Menu(menu)
-deskmenu = Menu(menu)
-helpmenu = Menu(menu)
-# file menu
-menu.add_cascade(label="Datei", menu=filemenu)
-filemenu.add_command(label="Neu")
-filemenu.add_command(label="Oeffnen")
-filemenu.add_command(label="Speichern")
-filemenu.add_command(label="Schliessen")
-# deskriptor menu
-menu.add_cascade(label="Deskriptor", menu=deskmenu)
-deskmenu.add_command(label="hinzufuegen")
-deskmenu.add_command(label="bearbeiten")
-deskmenu.add_command(label="loeschen")
+class Descriptorset(object):
 
-# deskriptor menu
-menu.add_cascade(label="Deskriptor", menu=helpmenu)
-helpmenu.add_command(label="hinzufuegen")
-helpmenu.add_command(label="bearbeiten")
-helpmenu.add_command(label="loeschen")
-root.mainloop()
+    def __init__(self, name):
+        self._name = name
+        self.dict = {}
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, newname):
+        self._name = newname
+
+    def get_terms(self):
+        return self.dict
+
+    def add_term(self, term, rel):
+        if rel in self.dict.keys():
+            self.dict[rel].append(term)
+        else:
+            self.dict[rel] = [term]
+
+    def edit_term(self, rel, term, newterm):
+        # for elem in self.dict[rel]:
+        #     print "edit:" + elem
+        #     if elem == term:
+        #         self.dict[rel][self.dict[rel].index(elem)] = newterm
+        for n, elem in enumerate(self.dict[rel]):
+            if elem == term:
+                self.dict[rel][n] = newterm
+
+    def remove_term(self, rel, term):
+        if rel in self.dict.keys():
+            self.dict[rel].remove(term)
+
+    def edit_rel(self, rel, term, newrel):
+        self.remove_term(rel, term)
+        self.add_term(term, newrel)
+
+descriptorset1 = Descriptorset("Auto")
+
+descriptorset1.add_term("Brumm", "VB")
+descriptorset1.add_term("Toeff", "VB")
+descriptorset1.edit_term("VB", "Toeff", "Tut")
+descriptorset1.remove_term("VB", "Tut")
+descriptorset1.edit_rel("VB", "Brumm", "OB")
+print descriptorset1.get_terms()
